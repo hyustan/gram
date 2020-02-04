455137be6cc23548c5bf2a98fbccef4f16c69e7b
Vahid <vd1371@gmail.com>
2020-02-04 17:07:40 +0800

    First commit on the sma branch

--- indicators/SMA.py
+++ indicators/SMA.py
@@ -1,5 +1,6 @@
 # Adding parent directory to the PYTHONPATH
 import sys
+import pandas as pd
 sys.path.insert(0,'..')
 from Indicator import Indicator
 from utils.GlobalVariables import *
@@ -7,10 +8,10 @@ from utils.GlobalVariables import *
 class SMA(Indicator):
 	# Simple moving average indicator
 	
-	def __init__(self, period = 14, of = CLOSE):
+	def __init__(self, period = 14, on = CLOSE):
 		super(SMA, self).__init__()
 		self.period = period
-		self.of = of
+		self.on = on
 
 	def calculate_(self):
 		'''
@@ -22,5 +23,8 @@ class SMA(Indicator):
 		pass
 
 if __name__ == "__main__":
+	df = pd.read_csv("..\data\Forex\GBPUSD\GBPUSD_1D.csv", index_col = 0)
+	print (df)
+	input()
 	my_SMA = SMA(20, CLOSE)
 	my_SMA.calculate()

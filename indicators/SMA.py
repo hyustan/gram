aa89b1d31abf7728f9eb9a118e89cea5b004083b
Vahid <vd1371@gmail.com>
2020-02-04 17:27:30 +0800

    finishing SMA indicator

--- indicators/SMA.py
+++ indicators/SMA.py
@@ -13,18 +13,24 @@ class SMA(Indicator):
 		self.period = period
 		self.on = on
 
-	def calculate_(self):
+	def calculate(self, df):
 		'''
 		returns the values of the simple moving average
 		:param: period: the period of the SMA indicator
-		:param: of: close, open, HLC
+		:param: on: close, open, HLC/3
 		:return: the SMA values as a panda series
 		'''
-		pass
+		df[HLC_3] = (df[HIGH] + df[LOW] + df[CLOSE])/3
+		out_series = df[self.on].rolling(window = self.period).mean()
+		
+		# For testing
+		'''
+		df['SMA(20)'] = out_series
+		df.to_csv("../test_reports/SMA_test/SMA20.csv")
+		'''
+		return out_series
 
 if __name__ == "__main__":
 	df = pd.read_csv("..\data\Forex\GBPUSD\GBPUSD_1D.csv", index_col = 0)
-	print (df)
-	input()
 	my_SMA = SMA(20, CLOSE)
-	my_SMA.calculate()
+	my_SMA.calculate(df)

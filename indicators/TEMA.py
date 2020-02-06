548c90b40bca460fd2cebb9381d71339e1b79d30
Vahid <vd1371@gmail.com>
2020-02-06 13:50:17 +0800

    first commit of tema

---
+++ indicators/TEMA.py
@@ -0,0 +1,31 @@
+# Adding parent directory to the PYTHONPATH
+import sys
+import pandas as pd
+sys.path.insert(0,'..')
+from Indicator import Indicator
+from utils.GlobalVariables import *
+
+class TEMA(Indicator):
+	# Simple moving average indicator
+	
+	def __init__(self, period = 14, on = CLOSE):
+		super(TEMA, self).__init__()
+		self.period = period
+		self.on = on
+
+	def calculate_(self):
+		'''
+		returns the values of the simple moving average
+		:param: period: the period of the SMA indicator
+		:param: of: close, open, HLC
+		:return: the SMA values as a panda series
+		'''
+		
+		
+
+if __name__ == "__main__":
+	df = pd.read_csv("..\data\Forex\GBPUSD\GBPUSD_1D.csv", index_col = 0)
+	print (df)
+	input()
+	my_TEMA = TEMA(14, CLOSE)
+	my_TEMA.calculate()

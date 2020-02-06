9327173b818fec806d1f0eaa0b5937d036c5fbaa
Vahid <vd1371@gmail.com>
2020-02-06 13:51:53 +0800

    minor modifications on ema

--- indicators/EMA.py
+++ indicators/EMA.py
@@ -6,7 +6,7 @@ from Indicator import Indicator
 from utils.GlobalVariables import *
 
 class EMA(Indicator):
-	# Simple moving average indicator
+	# Exponential moving average indicator
 	
 	def __init__(self, period = 14, on = CLOSE):
 		super(EMA, self).__init__()
@@ -16,9 +16,9 @@ class EMA(Indicator):
 	def calculate(self, df):
 		'''
 		returns the values of the simple moving average
-		:param: period: the period of the SMA indicator
+		:param: period: the period of the EMA indicator
 		:param: on: close, open, HLC/3
-		:return: the SMA values as a panda series
+		:return: the EMA values as a panda series
 		'''
 		df[HLC_3] = (df[HIGH] + df[LOW] + df[CLOSE])/3
 		out_series = df[self.on].ewm(span = self.period).mean()
@@ -32,4 +32,4 @@ class EMA(Indicator):
 if __name__ == "__main__":
 	df = pd.read_csv("..\data\Forex\GBPUSD\GBPUSD_1D.csv", index_col = 0)
 	my_ind = EMA(14, CLOSE)
-	my_ind.calculate(df)
+	print (my_ind.calculate(df))

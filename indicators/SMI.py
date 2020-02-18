5d7725436018909a57e027cfa1e3db18ec21eb2c
mahdibp7 <37873443+mahdibp7@users.noreply.github.com>
2020-02-18 11:44:10 -0800

    Comments addressed

--- indicators/SMI.py
+++ indicators/SMI.py
@@ -7,8 +7,7 @@ from utils.GlobalVariables import *
 
 class SMI(Indicator):
         # Stochastic Momentum Index
-        # the calculation is based on 
-
+        # the calculation is based on https://www.barchart.com/trader/help/studies/stoch_mom.php
         def __init__(self, period = 14, smoothing_period = 3):
                 super(SMI, self).__init__()
                 self.period = period
@@ -24,19 +23,23 @@ class SMI(Indicator):
 
                 hig_max = df[HIGH].rolling(window = self.period).max()
                 low_min = df[LOW].rolling(window = self.period).min()
-                center = (hig_max + low_min) / 2 # center of low to high range
-                dist2center = df[CLOSE] - center #subtract distance of Current Close from the Center of the Range.
-                dist2center_smoothed = dist2center.ewm(span = self.smoothing_period).mean() # smooth distance to center with an exponential moving average
+                # center of low to high range
+                center = (hig_max + low_min) / 2 
+                #subtract distance of Current Close from the Center of the Range.
+                dist2center = df[CLOSE] - center
+                # smooth distance to center with an exponential moving average
+                dist2center_smoothed = dist2center.ewm(span = self.smoothing_period).mean() 
                 dist2center_smoothed2 = dist2center_smoothed.ewm(span = self.smoothing_period).mean()
                 range_price = hig_max - low_min
-                range_smoothed = range_price.ewm(span = self.smoothing_period).mean() # smooth range with an exponential moving average
-                range_smoothed2 = range_smoothed.ewm(span = self.smoothing_period).mean()
-                out_series = dist2center_smoothed2/range_smoothed2
+                # smooth range with an exponential moving average
+                range_smoothed = range_price.ewm(span = self.smoothing_period).mean() 
+                range_smoothed2 = range_smoothed.ewm(span = self.smoothing_period).mean()/2
+                out_series = dist2center_smoothed2/range_smoothed2*100
 
                 return out_series
 	
 if __name__ == "__main__":
 	df = pd.read_csv("..\data\Forex\GBPUSD\GBPUSD_1D.csv", index_col = 0)
-	my_SMA = SMI(period = 14, smoothing_period = 3)
-	df['SMI(14,3)'] = my_SMA.calculate(df)
+	my_SMI = SMI(period = 14, smoothing_period = 3)
+	df['SMI(14,3)'] = my_SMI.calculate(df)
 	df.to_csv("SMI_test.csv")

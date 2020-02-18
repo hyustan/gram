1a0b3e270b988e64e2ab79e36b84c775b49617ea
mahdibp7 <37873443+mahdibp7@users.noreply.github.com>
2020-02-18 11:25:45 -0800

    Output changed to pd. Option of price added

--- indicators/FDI.py
+++ indicators/FDI.py
@@ -12,25 +12,26 @@ class FDI(Indicator):
     # and the webpage https://www.prorealcode.com/prorealtime-indicators/fractal-dimension-index-fdi/
     # I have put a picture from the formulation in test folder too
 
-    def __init__(self, period = 14):
+    def __init__(self, period = 14,on = CLOSE):
         super(FDI, self).__init__()
         self.period = period
-
+        self.on = on
     def calculate(self,df):
         '''
         returns the values of fractal dimension in a numpy array
         :param: period: the period of Fractal dimension
         '''
-        hig_max = df[HIGH].rolling(window = self.period).max()
-        low_min = df[LOW].rolling(window = self.period).min()
+        hig_max = df[CLOSE].rolling(window = self.period).max()
+        low_min = df[CLOSE].rolling(window = self.period).min()
         range_price = hig_max - low_min
         FDI = np.zeros(len(range_price))
-        for i in range(self.period,len(df[CLOSE])):
-            normalized_price = (df[CLOSE][i-self.period:i] - low_min[i])/range_price[i]
+        for i in range(self.period,len(df[self.on])):
+            normalized_price = (df[self.on][i-self.period:i] - low_min[i])/range_price[i]
             diff = np.diff(normalized_price)
             length = np.sqrt(diff**2+1/self.period**2)
             sum_length = np.sum(length)
             FDI[i] = 1+np.log(sum_length*2)/np.log(2*self.period)
+        FDI = pd.Series(FDI, index = df.index)
         return FDI
 
 if __name__ == "__main__":

0f491f1eb7d341a264356e947676828ec0874387
Vahid <vd1371@gmail.com>
2020-02-19 17:19:59 +0800

    finalizing the sma-ft

--- indicators_transformer/FT.py
+++ indicators_transformer/FT.py
@@ -3,17 +3,12 @@ import sys
 sys.path.insert(0,'..')
 from utils.GlobalVariables import *
 
-class SMA_FT(object):
+class FT(object):
 	# Base class for all of the feature transformers
 	def __init__(self):
-		super(SMA_FT, self).__init__()
+		super(FT, self).__init__()
 
-	def transform(self, df, features):
+	def transform(self, df):
 		# it construct a set of feature to be used in the GA, RL, or even static strategies
 		# We might need to modify this in the future
-		df['SMA'] = features
-		df['ft'] = df[df['']]
-
-
-
 		raise NotImplementedError ("'transform' feature is not ")

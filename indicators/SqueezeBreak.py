176dd2edec1cee87bf0d1886a1a9ff01e6d626ef
hamid <hamid.tehrani2003@gmail.com>
2020-02-24 12:58:16 +0330

    SqueezeBreak indicator transformer added

--- indicators/SqueezeBreak.py
+++ indicators/SqueezeBreak.py
@@ -2,7 +2,7 @@
 import sys
 import pandas as pd
 sys.path.insert(0,'..')
-from Indicator import Indicator
+from indicators.Indicator import Indicator
 from utils.GlobalVariables import *
 
 class SqueezeBreak(Indicator):

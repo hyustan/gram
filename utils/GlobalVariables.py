d34f22a5f45bc83394772856905e442e27d778b7
Vahid <vd1371@gmail.com>
2020-03-09 16:44:48 +0800

    adding closing reasons for trade to global variables

--- utils/GlobalVariables.py
+++ utils/GlobalVariables.py
@@ -12,3 +12,9 @@ LONG, SHORT, WAIT , TRADE = 1, -1, 0, 1
 
 # All pairs # TODO: to be completed
 GBPUSD = 'GBPUSD'
+
+
+# Reasons for closing trades
+TOUCH_SL = 'touch_stop_loss'
+OPPOSITE_SIGNAL = 'opposite_signal'
+TRAILING_SL = 'trailing_stop_loss'

f66c33dd5a48721b9ea9c26562ead3af9caffe57
Vahid <vd1371@gmail.com>
2020-03-09 17:14:24 +0800

    loading report test

--- trader/BaseTrader.py
+++ trader/BaseTrader.py
@@ -10,11 +10,11 @@ from datetime import datetime
 class BaseTrader(object):
 	# Base class for creating trader agents
 	# This is a very rough idea, it must be completed in the future
-	def __init__(self):
-		super(BaseTrader, self).__init__(pair_name= 'UNSPECIFIED')
+	def __init__(self, pair_name= 'UNSPECIFIED'):
+		super(BaseTrader, self).__init__()
 
 		# This is the name of the report folder, all reports including graphs, analysis, etc. will be saved under this name
-		name = pair_name + "-" str(datetime.now())[:-10].replace(":", "-")
+		name = pair_name + "-" + str(datetime.now())[:-10].replace(":", "-")
 
 		# Creating the report directory
 		self.report_dir = f"../test_reports/Report-{name}"
@@ -40,11 +40,27 @@ class BaseTrader(object):
 	def save_trade_history(self):
 		# This method saves the report created by the trader in a csv file
 		# The file should be similar to the MT4 trade history
-
+		pass
 
 
 if __name__ == '__main__':
-	1
+	import pandas as pd
+	df = pd.read_csv('TestForBaseTrader.csv', index_col =0)
+
+	def date_parser_1(date_as_str):
+		# For the case index: 5/12/1993 0:00
+		# There might be other cases so I named it date_parser_1
+		return datetime.strptime(date_as_str, "%m/%d/%Y %H:%M")
+
+	df['opening_time'].apply(date_parser_1)
+	df['closing_time'].apply(date_parser_1)
+
+	myBaseTrader = BaseTrader()
+	myBaseTrader.report = df
+
+	print (myBaseTrader.report)
+
+
 
 
 

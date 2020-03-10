b5030c99351aaccc305e9eef18c0b4ef020c9161
Vahid <vd1371@gmail.com>
2020-03-10 16:01:44 +0800

    Modifying dms

--- decision_models/VP.py
+++ decision_models/VP.py
@@ -17,10 +17,23 @@ class VP(BaseDecisionModel):
 		pass
 
 	def predict_value(self, inp):
-		return 1/3
+		pass
 
-	def decide(self):
-		return np.random.randint(3, size=len(inp)) - 1
+	def decide(self, X, X_1, close_price, ATR, win_previous_trade):
+		# Assumptions:
+		'''
+		X_1 is the previous feature vector
+		X0 = baseline
+		X1 = first_trend
+		X2 = second_trend
+		X3 = Volume
+		'''
+
+		if X[0]*X_1[0] < 0 and abs(close_price-X[0])<1.5*ATR and 
+
+	def should_close(self, X):
+		
+
 
 	def save(self):
 		pass
